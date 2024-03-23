ecd8fe4c193a8ee842ad840bc83766422d705e15
Rafał Laskowski <rafal.laskowski02@gmail.com>
2024-03-23 10:48:54 +0100

    adding branch

--- backend/src/main/java/org/io_web/backend/server/Server.java
+++ backend/src/main/java/org/io_web/backend/server/Server.java
@@ -1,11 +1,25 @@
 package org.io_web.backend.server;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Needed to handle connection before the game start
  */
+@RestController
 public class Server implements Runnable{
     private ClientPool clientPool;
     private GameEngine gameEngine;
+
+    public Server() {
+        this.gameEngine = new GameEngine();
+        this.clientPool = new ClientPool();
+    }
+
+    @GetMapping("/lobby")
+    public void startGame() {
+
+    }
 
     @Override
     public void run() {
