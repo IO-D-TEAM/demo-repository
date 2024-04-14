069c9b1e17af569af67f3ba6aa0d5c408abecae2
Grzegorz <grzegorzpiskorski971@gmail.com>
2024-04-14 22:24:44 +0200

    IO-54

---
+++ frontend/src/model/GameView/Question/QuestionPopUp.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./QuestionPopUp.css";
+import { Question } from "../../../interfaces/GameViewInterfaces/Question";
+
+
+interface Props {
+    question: Question;
+}
+
+interface GridPos {
+    row: number;
+    column: number;
+    answer: string;
+}
+
+const QuestionPopUp = (props: Props) => {
+    const gridPositions: GridPos[] = [
+        {row: 1, column: 1, answer: "A"}, {row: 1, column: 2, answer: "B"},
+        {row: 2, column: 1, answer: "C"}, {row: 2, column: 2, answer: "D"}
+    ];
+
+    // Just like all css on this board it's crap but hopefull it works
+    const gridShape: React.CSSProperties = {
+        gridTemplateRows: `repeat(${props.question.answers.length > 2 ? 2 : 1}, 1fr)`,
+        gridTemplateColumns: `repeat(2, 1fr)`
+    };
+    
+    return (
+        <div className="question-wrap">
+            <div className="question">
+                <h1>{props.question.question}</h1>
+            </div>
+            <div className="answers" style={gridShape}>
+                {
+                    props.question.answers.map((answer, i) => (
+                        <div className="answer" style={{
+                            gridRow: gridPositions[i].row,
+                            gridColumn: gridPositions[i].column
+                        }}>
+                            <h3>{`${gridPositions[i].answer}: ${answer}`}</h3>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+}
+
+
+export default QuestionPopUp;
