df739f4fada35cc39aaae9427a2ea7dc26607693
dominiks01 <dominikszot950@gmail>
2024-04-16 00:11:24 +0200

    and some changes

--- frontend/src/model/TeacherView/CreateQuestionsSet/QuestionBoard/QuestionBoard.tsx
+++ frontend/src/model/TeacherView/CreateQuestionsSet/QuestionBoard/QuestionBoard.tsx
@@ -1,36 +1,40 @@
-import React, { FC, useState, useEffect, useContext } from 'react';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import List from '@mui/material/List';
-import Button from '@mui/material/Button';
-import './QuestionBoard.css'
-import DeleteIcon from '@mui/icons-material/Delete';
-import IconButton from '@mui/material/IconButton';
-import QuestionValidationService from '../../../../services/QuestionsCreating/QuestionValidator';
-// import QuestionService from './../../../../services/QuestionsCreating/QuestionsCreatingService';
-import {QuestionInterface} from "./../../../../interfaces/QuestionInterfaces/Question"
+import React, { FC, useState, useEffect, useContext } from "react";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import List from "@mui/material/List";
+import Button from "@mui/material/Button";
 import "./QuestionBoard.css";
-import { useQuestionService }  from './../../../../services/QuestionsCreating/QuestionsCreatingService';
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import QuestionValidationService from "../../../../services/QuestionsCreating/QuestionValidator";
+import { QuestionInterface } from "./../../../../interfaces/QuestionInterfaces/Question";
+import "./QuestionBoard.css";
+import { useQuestionService } from "./../../../../services/QuestionsCreating/QuestionsCreatingService";
 
-interface QuestionBoardProps {
+interface QuestionBoardProps {}
+
+{
+  /* Component that will show all questions */
 }
-
-{/* Component that will show all questions */ }
 export const QuestionBoard: FC<QuestionBoardProps> = () => {
   const [questions, setQuestions] = useState<QuestionInterface[]>([]);
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   let [rerenderKey, setRerenderKey] = useState<number>(0); // State variable to trigger rerender
   const questionService = useQuestionService(); // Access the QuestionService instance
 
-  {/* Subscribeses for changes in QuestionService, 
+  {
+    /* Subscribeses for changes in QuestionService, 
     and gets question list. Main functionality is 
-    to subscribe service to know when edited question is changed.   */}
+    to subscribe service to know when edited question is changed.   */
+  }
   useEffect(() => {
     const handleQuestionChanges = (questions: QuestionInterface[]) => {
       setQuestions(questions);
       setSelectedIndex(questionService.getActualIndex());
-      setRerenderKey(prevKey => prevKey === rerenderKey ? rerenderKey++ : rerenderKey++);
+      setRerenderKey((prevKey) =>
+        prevKey === rerenderKey ? rerenderKey++ : rerenderKey++
+      );
       rerenderKey %= 100;
     };
 
@@ -42,44 +46,44 @@ export const QuestionBoard: FC<QuestionBoardProps> = () => {
     };
   }, [questionService]);
 
-   {/* Change Actual Edited Question on click   */}
-  const handleListItemClick = (
-    index: number,
-  ) => {
-    // setSelectedIndex(index);
+  {
+    /* Change Actual Edited Question on click   */
+  }
+  const handleListItemClick = (index: number) => {
     questionService.setActualQuestion(questions[index], index);
   };
 
-  const handleNewQuestion = (() => {
-    questionService.addQuestion();  // Add new empty question onClick 
-  })
+  const handleNewQuestion = () => {
+    questionService.addQuestion(); // Add new empty question onClick
+  };
 
-  const handleDeleteQuestion = ((
-    question: QuestionInterface ) => {
-      questionService.removeQuestion(question);  // Delete question
-  })
+  const handleDeleteQuestion = (question: QuestionInterface) => {
+    questionService.removeQuestion(question); // Delete question
+  };
 
   // Mark invalid questions on board
-  const simpleValidate = ((
-    question: QuestionInterface, questions: QuestionInterface[]) => {
-      try{
-        QuestionValidationService.validateQuestion(question, questions);
-        return true;
-      } catch(error){
-        if(error instanceof Error){
-          return false;
-        }
+  const simpleValidate = (
+    question: QuestionInterface,
+    questions: QuestionInterface[]
+  ) => {
+    try {
+      QuestionValidationService.validateQuestion(question, questions);
+      return true;
+    } catch (error) {
+      if (error instanceof Error) {
+        return false;
       }
-    });
+    }
+  };
 
   return (
-    <div >
+    <div>
       <Button
         variant="contained"
         color="primary"
         onClick={handleNewQuestion} // Add a function to handle adding a new question
-        sx={{ 
-          width: '100%',
+        sx={{
+          width: "100%",
         }}
       >
         Dodaj nowe pytanie
@@ -87,40 +91,46 @@ export const QuestionBoard: FC<QuestionBoardProps> = () => {
 
       <List
         key={rerenderKey} // Use the service as key to force re-render when it changes
-        sx={{ 
+        sx={{
           // position: 'relative',
-          marginTop: '5px',
-          paddinngTop: '5px',
-          overflow: 'auto',
-          maxHeight: '620px'
+          marginTop: "5px",
+          paddinngTop: "5px",
+          overflow: "auto",
+          maxHeight: "620px",
         }}
         aria-label="contacts"
       >
         {questions.map((question, index) => (
-          <ListItem key={index} disablePadding 
+          <ListItem
+            key={index}
+            disablePadding
             secondaryAction={
-              <IconButton 
-                edge="end" 
-                aria-label="delete" 
+              <IconButton
+                edge="end"
+                aria-label="delete"
                 onClick={() => handleDeleteQuestion(question)}
               >
                 <DeleteIcon />
               </IconButton>
-            }>
-            <ListItemButton 
+            }
+          >
+            <ListItemButton
               onClick={() => handleListItemClick(index)}
+              autoFocus={index == questionService.getActualIndex()}
               selected={index == questionService.getActualIndex()}
             >
-              <ListItemText 
+              <ListItemText
                 className="questionBoardItem"
-                primary={question.question} 
-                style={{color: simpleValidate(question, questions)? "green": "red"}} 
+                primary={question.question}
+                style={{
+                  color: simpleValidate(question, questions) ? "green" : "red",
+                }}
               />
             </ListItemButton>
-            
           </ListItem>
         ))}
       </List>
-    </div>);
-}
+    </div>
+  );
+};
 export default QuestionBoard;
--- frontend/src/services/QuestionsCreating/QuestionsCreatingService.tsx
+++ frontend/src/services/QuestionsCreating/QuestionsCreatingService.tsx
@@ -1,189 +1,218 @@
-import React, { createContext, useContext, ReactNode, useState, useEffect } from 'react';
+import React, {
+  createContext,
+  useContext,
+  ReactNode,
+  useState,
+  useEffect,
+} from "react";
 import { QuestionInterface } from "../../interfaces/QuestionInterfaces/Question";
-import {QuestionService, SubscriberCallback } from "../../interfaces/QuestionInterfaces/QuestionService"; 
+import {
+  QuestionService,
+  SubscriberCallback,
+} from "../../interfaces/QuestionInterfaces/QuestionService";
 
-const QuestionServiceContext = createContext<QuestionService | undefined>(undefined);
+const QuestionServiceContext = createContext<QuestionService | undefined>(
+  undefined
+);
 
-// This is to allow components to use our service 
+// This is to allow components to use our service
 export const useQuestionService = () => {
-    const context = useContext(QuestionServiceContext);
-    if (!context) {
-        throw new Error('useQuestionService must be used within a QuestionServiceProvider');
-    }
-    return context;
+  const context = useContext(QuestionServiceContext);
+  if (!context) {
+    throw new Error(
+      "useQuestionService must be used within a QuestionServiceProvider"
+    );
+  }
+  return context;
 };
 
 interface QuestionServiceProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 const mockQuestion: QuestionInterface = {
-    question: "Przykładowe pytanie",
-    correctAnswer: "Odpowiedź 1",
-    answers: ["Odpowiedź 1", "Odpowiedź 2", "Odpowiedź 3", "Odpowiedź 4"]
-}
+  question: "Przykładowe pytanie",
+  correctAnswer: "Odpowiedź 1",
+  answers: ["Odpowiedź 1", "Odpowiedź 2", "Odpowiedź 3", "Odpowiedź 4"],
+};
 
-const initialState: QuestionInterface[] = [
-    
-];
+const initialState: QuestionInterface[] = [];
 
 // Main service functionality
-export const QuestionServiceProvider: React.FC<QuestionServiceProviderProps> = ({ children }) => {
-    const [questions, setQuestionsValue] = useState<QuestionInterface[]>(initialState); // Your initial questions data
-    const [actualQuestion, setActualQuestionValue] = useState<QuestionInterface>(structuredClone(mockQuestion)); // Your initial questions data
-    const [index, setIndex] = useState<number>(-1);
+export const QuestionServiceProvider: React.FC<
+  QuestionServiceProviderProps
+> = ({ children }) => {
+  const [questions, setQuestionsValue] =
+    useState<QuestionInterface[]>(initialState); // Your initial questions data
+  const [actualQuestion, setActualQuestionValue] = useState<QuestionInterface>(
+    structuredClone(mockQuestion)
+  ); // Your initial questions data
+  const [index, setIndex] = useState<number>(-1);
 
-    let subscribers: { callback: SubscriberCallback, dataIdentifier: string }[] = [];
+  let subscribers: { callback: SubscriberCallback; dataIdentifier: string }[] =
+    [];
 
-    useEffect(() => {
-        if(questions.length === 0)
-            setActualQuestionValue(structuredClone(mockQuestion));
-        notifySubscribers();
-    }, [questions, actualQuestion]);
+  useEffect(() => {
+    if (questions.length === 0 && index != -1)
+      setActualQuestionValue(structuredClone(mockQuestion));
+    notifySubscribers();
+  }, [questions, actualQuestion]);
 
-    const notifySubscribers = () => {
-        subscribers.forEach(subscriber => {
-        if (subscriber.dataIdentifier === "question")
-            if(actualQuestion)
-                subscriber.callback(actualQuestion);
-        
-        if (subscriber.dataIdentifier === "questions")
-            if(questions)
-                subscriber.callback(questions);
-        });
-    };
+  const notifySubscribers = () => {
+    subscribers.forEach((subscriber) => {
+      if (subscriber.dataIdentifier === "question")
+        if (actualQuestion) subscriber.callback(actualQuestion);
 
-    const subscribe = (callback: SubscriberCallback, dataIdentifier: string) => {
-        subscribers.push({ callback, dataIdentifier });
-    };
+      if (subscriber.dataIdentifier === "questions")
+        if (questions) subscriber.callback(questions);
+    });
+  };
 
-    const unsubscribe = (callback: SubscriberCallback) => {
-        subscribers = subscribers.filter(subscriber => subscriber.callback !== callback);
-    };
+  const subscribe = (callback: SubscriberCallback, dataIdentifier: string) => {
+    subscribers.push({ callback, dataIdentifier });
+  };
 
-    const getQuestions = (): QuestionInterface[] => {
-        return questions;
-    }
-
-    const getActualIndex = (): number  => {
-        return index;
-    }
-
-    const setActualQuestion = (question: QuestionInterface, newIndex: number): void => {
-        setIndex(newIndex);
-        setActualQuestionValue(question);
-    }
-
-    const getActualQuestion = (): QuestionInterface => {
-        if(actualQuestion == null || questions.length === 0)
-            return structuredClone(mockQuestion);
-        
-        return actualQuestion;
-    }
-
-    const saveChanges = () : void  => {
-        if(!checkIfQuestionExists(actualQuestion)) // If question exist, ignore it
-            questions.push(structuredClone(actualQuestion));
-
-        notifySubscribers();
-    }
-
-    const setQuestions = (newQuestions: QuestionInterface[]): void  => {
-        setQuestionsValue(newQuestions);
-
-        if(newQuestions.length == 0)
-            setActualQuestion(structuredClone(mockQuestion), -1);
-
-    }
-
-    const addQuestion = () : void => {
-        if(index == -1){
-            notifySubscribers();
-            return;
-        }
-
-        setIndex(-1);
-        setActualQuestionValue(structuredClone(mockQuestion));
-    }
-
-    const updateCorrectAnswer = (correctAnswer: string) : void => {
-        setActualQuestionValue(prevQuestion => ({ ...prevQuestion!, correctAnswer: correctAnswer}))
-        if (index !== -1 && questions.length > index) {
-            setQuestionsValue(prevQuestions => {
-                const updatedQuestions = [...prevQuestions];
-                updatedQuestions[index].correctAnswer = correctAnswer;
-                return updatedQuestions;
-            });
-        }
-    }
-
-    const checkIfQuestionExists = (question: QuestionInterface) : boolean => {
-
-        if(questions.length == 0)
-            return false;
-
-        let copiedQuestions = [...questions]; // Create a copy of the original array
-        return copiedQuestions.findIndex(q => q.question === question.question) !== -1;
-    }
-
-    const updateQuestionAnswers = (newAnswers: string[]) : void => {
-        setActualQuestionValue(prevQuestion => ({ ...prevQuestion!, answers: newAnswers}))
-        if (index !== -1 && questions.length > index) {
-            setQuestionsValue(prevQuestions => {
-                const updatedQuestions = [...prevQuestions];
-                updatedQuestions[index].answers = newAnswers;
-                return updatedQuestions;
-            });
-        }
-    }
-
-    const updateQuestionValue = (newQuestion: string) : void => {
-        console.log(newQuestion, actualQuestion, index);
-
-        if (index != -1) {
-            setQuestionsValue(prevQuestions => {
-                const updatedQuestions = [...prevQuestions];
-                updatedQuestions[index].question = newQuestion;
-                return updatedQuestions;
-            });
-        }
-
-        setActualQuestionValue(prevQuestion => ({ ...prevQuestion!, question: newQuestion}))
-    }
-
-    const removeQuestion = (question: QuestionInterface): void => {
-        const updatedQuestions = [...questions];
-        const questionIndex = questions.indexOf(question);
-        updatedQuestions.splice(questionIndex, 1);
-        console.log("Correct", updatedQuestions);
-        setQuestions(updatedQuestions);
-
-        if(question == actualQuestion){
-            setActualQuestion( structuredClone(mockQuestion), -1);
-        } else if(questionIndex< index ){
-            setIndex(index-1);
-        }
-    };
-
-    const service: QuestionService = {
-        subscribe,
-        unsubscribe,
-        getQuestions,
-        setActualQuestion,
-        removeQuestion,
-        addQuestion,
-        updateQuestionValue,
-        updateQuestionAnswers,
-        updateCorrectAnswer,
-        setQuestions,
-        saveChanges,
-        getActualQuestion,
-        getActualIndex
-    };
-
-    return (
-        <QuestionServiceContext.Provider value={service}>
-            {children}
-        </QuestionServiceContext.Provider>
+  const unsubscribe = (callback: SubscriberCallback) => {
+    subscribers = subscribers.filter(
+      (subscriber) => subscriber.callback !== callback
     );
+  };
+
+  const getQuestions = (): QuestionInterface[] => {
+    return questions;
+  };
+
+  const getActualIndex = (): number => {
+    return index;
+  };
+
+  const setActualQuestion = (
+    question: QuestionInterface,
+    newIndex: number
+  ): void => {
+    setIndex(newIndex);
+    setActualQuestionValue(question);
+  };
+
+  const getActualQuestion = (): QuestionInterface => {
+    if (actualQuestion == null || questions.length === 0)
+      return structuredClone(mockQuestion);
+
+    return actualQuestion;
+  };
+
+  const saveChanges = (): void => {
+    if (!checkIfQuestionExists(actualQuestion))
+      // If question exist, ignore it
+      questions.push(structuredClone(actualQuestion));
+
+    notifySubscribers();
+  };
+
+  const setQuestions = (newQuestions: QuestionInterface[]): void => {
+    setQuestionsValue(newQuestions);
+
+    if (newQuestions.length == 0)
+      setActualQuestion(structuredClone(mockQuestion), -1);
+  };
+
+  const addQuestion = (): void => {
+    if (index == -1) {
+      notifySubscribers();
+      return;
+    }
+
+    setIndex(-1);
+    setActualQuestionValue(structuredClone(mockQuestion));
+  };
+
+  const updateCorrectAnswer = (correctAnswer: string): void => {
+    setActualQuestionValue((prevQuestion) => ({
+      ...prevQuestion!,
+      correctAnswer: correctAnswer,
+    }));
+    if (index !== -1 && questions.length > index) {
+      setQuestionsValue((prevQuestions) => {
+        const updatedQuestions = [...prevQuestions];
+        updatedQuestions[index].correctAnswer = correctAnswer;
+        return updatedQuestions;
+      });
+    }
+  };
+
+  const checkIfQuestionExists = (question: QuestionInterface): boolean => {
+    if (questions.length == 0) return false;
+
+    let copiedQuestions = [...questions]; // Create a copy of the original array
+    return (
+      copiedQuestions.findIndex((q) => q.question === question.question) !== -1
+    );
+  };
+
+  const updateQuestionAnswers = (newAnswers: string[]): void => {
+    setActualQuestionValue((prevQuestion) => ({
+      ...prevQuestion!,
+      answers: newAnswers,
+    }));
+    if (index !== -1 && questions.length > index) {
+      setQuestionsValue((prevQuestions) => {
+        const updatedQuestions = [...prevQuestions];
+        updatedQuestions[index].answers = newAnswers;
+        return updatedQuestions;
+      });
+    }
+  };
+
+  const updateQuestionValue = (newQuestion: string): void => {
+    console.log(newQuestion, actualQuestion, index);
+
+    if (index != -1) {
+      setQuestionsValue((prevQuestions) => {
+        const updatedQuestions = [...prevQuestions];
+        updatedQuestions[index].question = newQuestion;
+        return updatedQuestions;
+      });
+    }
+
+    setActualQuestionValue((prevQuestion) => ({
+      ...prevQuestion!,
+      question: newQuestion,
+    }));
+  };
+
+  const removeQuestion = (question: QuestionInterface): void => {
+    const updatedQuestions = [...questions];
+    const questionIndex = questions.indexOf(question);
+    updatedQuestions.splice(questionIndex, 1);
+    console.log("Correct", updatedQuestions);
+    setQuestions(updatedQuestions);
+
+    if (question == actualQuestion) {
+      setActualQuestion(structuredClone(mockQuestion), -1);
+    } else if (questionIndex < index) {
+      setIndex(index - 1);
+    }
+  };
+
+  const service: QuestionService = {
+    subscribe,
+    unsubscribe,
+    getQuestions,
+    setActualQuestion,
+    removeQuestion,
+    addQuestion,
+    updateQuestionValue,
+    updateQuestionAnswers,
+    updateCorrectAnswer,
+    setQuestions,
+    saveChanges,
+    getActualQuestion,
+    getActualIndex,
+  };
+
+  return (
+    <QuestionServiceContext.Provider value={service}>
+      {children}
+    </QuestionServiceContext.Provider>
+  );
 };
